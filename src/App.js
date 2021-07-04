53aafef3d627861713c23462427a9de3dea82983
jessica you <jessicaxytan@gmail.com>
2021-07-04 17:17:06 -0400

    commit tree 7f7ff8a6989663586c6a68980fe05ab133e498cc

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    state ={
+        listOfPeople: []
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.listOfPeople.map(
+                    person => person.name
+                )}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(data => this.setState({listOfPeople: data.people}))
+    }
+
+}
+
+export default App
